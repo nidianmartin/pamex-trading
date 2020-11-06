06c9568e1b04a386dce4ed5bec9530d4bc3d766e
Nidian Martin <nidianmartin@gmail.com>
2020-11-06 13:53:42 +0100

    Cambio en services

--- src/services/api-services.js
+++ src/services/api-services.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-axios.defaults.baseURL = "http://localhost:3001";
+axios.defaults.baseURL = `${process.env.REACT_APP_API_URL}`;
 axios.defaults.withCredentials = true;
 
 export const login = ({email, password}) => {
