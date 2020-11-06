9409ad02c5b9d51f4511e1eb8a91274bf751e518
Nidian Martin <nidianmartin@gmail.com>
2020-11-06 19:04:17 +0100

    hola

--- src/components/Chatbot/Chatbot.jsx
+++ src/components/Chatbot/Chatbot.jsx
@@ -1,33 +1,34 @@
 import React, { useState } from "react";
-import Chatbot from "react-chatbot-kit";
-import ActionProvider from "./ActionProvider";
-import MessageParser from "./MessageParser";
-import config from "./Config";
+// import Chatbot from "react-chatbot-kit";
+// import ActionProvider from "./ActionProvider";
+// import MessageParser from "./MessageParser";
+// import config from "./Config";
 import { VscHubot } from 'react-icons/vsc'
 import './chatbot.css'
 
 export default function ChatBot() {
   const [showBot, toggleBot] = useState(false);
-  const saveMessages = (messages) => {
-    localStorage.setItem("chat_messages", JSON.stringify(messages));
-  };
+  // const saveMessages = (messages) => {
+  //   localStorage.setItem("chat_messages", JSON.stringify(messages));
+  // };
 
-  const loadMessages = () => {
-    const messages = JSON.parse(localStorage.getItem("chat_messages"));
-    return messages;
-  };
+  // const loadMessages = () => {
+  //   const messages = JSON.parse(localStorage.getItem("chat_messages"));
+  //   return messages;
+  // };
 
   return (
     <div>
       {showBot && (
         <div className="chatbot-content">
-          <Chatbot
+          HOLA
+          {/* <Chatbot
             config={config}
             actionProvider={ActionProvider}
             messageParser={MessageParser}
             messageHistory={loadMessages()}
             saveMessages={saveMessages}
-          />
+          /> */}
         </div>
         )}
         <button className="app-chatbot-button" onClick={() => toggleBot((prev) => !prev)}><VscHubot style={{fontSize:"40px"}}/></button>
