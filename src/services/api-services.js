aef1a2a0517aea7c3a57a7c6f489324a97286a77
Nidian Martin <nidianmartin@gmail.com>
2020-11-06 18:12:15 +0100

    Cambio en services

--- src/services/api-services.js
+++ src/services/api-services.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-axios.defaults.baseURL = `${process.env.REACT_APP_API_URL}`;
+axios.defaults.baseURL = 'https://pamex-api.herokuapp.com/'
 axios.defaults.withCredentials = true;
 
 export const login = ({email, password}) => {
