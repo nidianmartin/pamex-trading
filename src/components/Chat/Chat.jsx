3f6a166de5186e932ec4e9b156529e110e02cacd
Nidian Martin <nidianmartin@gmail.com>
2020-11-06 19:28:45 +0100

    chatbot

--- src/components/Chat/Chat.jsx
+++ src/components/Chat/Chat.jsx
@@ -2,7 +2,91 @@ import React, { useState } from "react";
 import Chatbot from "react-chatbot-kit";
 import { VscHubot } from 'react-icons/vsc'
 import './chatbot.css'
-import config from "../ChatBot/Config";
+//Config
+import { createChatBotMessage } from "react-chatbot-kit";
+import BotAvatar from "../ChatBot/BotAvatar/BotAvatar";
+import UserAvatar from "../ChatBot/UserAvatar/UserAvatar";
+import LearningOptions from "../ChatBot/LearningOptions/LearningOptions";
+import LinkList from "../ChatBot/LinkList/LinkList";
+
+
+const config = {
+  initialMessages: [
+    createChatBotMessage(
+      `Hola soy Mets, Bienvenidos a la presentación de Nidian. Sere tu guía en este mundo de criptomedas. ¿En que te puedo ayudar?`,
+      {
+        widget: "learningOptions",
+      }
+      ),
+    ],
+    botName: "Mets",
+    customComponents : {
+      botAvatar: (props) => <BotAvatar {...props}/>,
+      userAvatar: (props) => <UserAvatar {...props}/>,
+    },
+    customStyles: {
+    botMessageBox: {
+      backgroundColor: "#2676e4",
+    },
+    chatButton: {
+      backgroundColor: "#37d0e0",
+    },
+  },
+  widgets: [
+    {
+      widgetName: "learningOptions",
+      widgetFunc: (props) => <LearningOptions {...props} />,
+    },
+    {
+      widgetName: "cryptoLinks",
+      widgetFunc: (props) => <LinkList {...props} />,
+      props: {
+        options: [
+          {
+            text: "Crytocurrencies Wikipedia",
+            url:
+              "https://es.wikipedia.org/wiki/Criptomoneda",
+            id: 1,
+          },
+          {
+            text: "Lo que hay que saber sobre las criptomonedas",
+            url:
+              "https://www.consumidor.ftc.gov/articulos/lo-que-hay-que-saber-sobre-las-criptomonedas",
+            id: 2,
+          },
+        ],
+      },
+    },
+    {
+      widgetName: "blockLinks",
+      widgetFunc: (props) => <LinkList {...props} />,
+      props: {
+        options: [
+          {
+            text: "Blockchain es...",
+            url:
+              "https://www.xataka.com/especiales/que-es-blockchain-la-explicacion-definitiva-para-la-tecnologia-mas-de-moda",
+            id: 1,
+          },
+        ],
+      },
+    },
+    {
+      widgetName: "saleCurrencies",
+      widgetFunc: (props) => <LinkList {...props} />,
+      props: {
+        options: [
+          {
+            text: "Exchanges",
+            url:
+              "/exchanges",
+            id: 1,
+          },
+        ],
+      },
+    },
+  ],
+};
 
 // ActionProvider starter code
 class ActionProvider {
